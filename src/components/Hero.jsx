fa13cd05c578cdb99c0fe1371466880499281b73
Anmol Dube <114559810+Anmoldube@users.noreply.github.com>
2024-03-02 22:23:57 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop,  <br className='sm:block hidden' />
-            Software and web applications And Cybersecurty Project
+            Software and web applications And CyberSecurity Project
           </p>
         </div>
       </div>
